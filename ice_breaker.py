dad3824066aaa035b707db34327d75ede5f23690
jeetbafna <jeetanil97@gmail.com>
2024-03-08 23:00:28 -0600

    Creating a prompt and using a llm

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,7 +1,29 @@
 from dotenv import load_dotenv
 import os
+from langchain_community.chat_models import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+from langchain.chains import LLMChain
 
 if __name__ == "__main__":
     print("Hello LangChain")
     load_dotenv()
-    print(os.environ["COOL_API_KEY"])
+
+    information = """
+    Elon Reeve Musk (/ˈiːlɒn/; EE-lon; born June 28, 1971) is a businessman and investor. He is the founder, chairman, CEO, and CTO of SpaceX; angel investor, CEO, product architect, and former chairman of Tesla, Inc.; owner, executive chairman, and CTO of X Corp.; founder of the Boring Company and xAI; co-founder of Neuralink and OpenAI; and president of the Musk Foundation. He is one of the wealthiest people in the world, with an estimated net worth of US$213 billion as of February 2024, according to the Bloomberg Billionaires Index, and $210 billion according to Forbes, primarily from his ownership stakes in Tesla and SpaceX.[5][6]
+    """
+
+    summary_template = """
+    Given the information {information} about a person I want you to create:
+    1. A short summary
+    2. Two interesting facts about them
+    """
+
+    summary_prompt_template = PromptTemplate(input_variables=["information"], template= summary_template)
+
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+
+    chain = LLMChain(llm=llm, prompt=summary_prompt_template)
+
+    res = chain.invoke(input={"information": information})
+
+    print(res)
