4b81b200657d99d8c219489bcda7d80b9990b289
jeetbafna <jeetanil97@gmail.com>
2024-03-09 20:58:17 -0600

    Created agent skeleton

---
+++ agents/linkedin_lookup_agent.py
@@ -0,0 +1,30 @@
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain.agents import initialize_agent, Tool, AgentType
+
+
+def lookup(name: str) -> str:
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+    template = """given the full name {name_of_person} I want you to get me a link to their Linkedin profile page.
+        Your answer should contain only a URL"""
+
+    tools_for_agent = [
+        Tool(
+            name="Crawl Google for linkedin profile page",
+            func="",
+            description="useful for when you need to get the LinkedIn profile URL",
+        )
+    ]
+
+    agent = initialize_agent(
+        tools=tools_for_agent,
+        llm=llm,
+        agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+        verbose=True,
+    )
+    prompt_template = PromptTemplate(
+        template=template, input_variables=["name_of_person"]
+    )
+
+    linked_profile_url = agent.run(prompt_template.format_prompt(name_of_person=name))
+    return linked_profile_url
--- ice_breaker.py
+++ ice_breaker.py
@@ -21,10 +21,11 @@ if __name__ == "__main__":
     2. Two interesting facts about them
     """
 
-    summary_prompt_template = PromptTemplate(input_variables=["information"], template= summary_template)
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], template=summary_template
+    )
 
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
-
 
     chain = LLMChain(llm=llm, prompt=summary_prompt_template)
 
